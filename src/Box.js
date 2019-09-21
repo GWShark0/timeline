c6d8f513c564f6ca42ece5f04300800519ade597
Greg Douglas <greg@storyblocks.com>
2019-09-20 23:20:26 -0400

    Dragging without bounds or slack

--- src/Box.js
+++ src/Box.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DraggableCore } from 'react-draggable';
+import clamp from 'lodash/clamp';
 import { ReactComponent as DragHandle } from './assets/drag-handle.svg';
 
 import './Box.scss';
 
 function Box() {
+  const [position, setPosition] = useState('');
+  const [left, setLeft] = useState(50);
+  const [width, setWidth] = useState(200);
+
+
+  const onDragStart = (event) => {
+    setPosition(event.target.dataset.position);
+  };
+
+  const onDrag = (event, data) => {
+    const { deltaX } = data;
+
+    if (position === 'left') {
+      if (width - deltaX > 60) {
+        setLeft(left + deltaX);
+        setWidth(width - deltaX);
+      }
+
+    } else if (position === 'right') {
+      setWidth(clamp(width + deltaX, 60, Infinity));
+    } else {
+      setLeft(left + deltaX);
+    }
+
+  };
+
+  const onDragStop = () => {
+    setPosition('');
+  };
+
+  const style = { left, width };
+
   return (
-    <div className="box">
-      <DragHandle className="box__handle" data-position="left" />
-      <DragHandle className="box__handle" data-position="right" />
-    </div>
+
+    <DraggableCore
+      onStart={onDragStart}
+      onDrag={onDrag}
+      onStop={onDragStop}
+      handle=".box__handle"
+    >
+      <div className="box" style={style}>
+        <div className="box__handle" data-position="left">
+          <DragHandle />
+        </div>
+        <div className="box__handle" data-position="center" />
+        <div className="box__handle" data-position="right">
+          <DragHandle />
+        </div>
+      </div>
+    </DraggableCore>
+
   )
 }
 
